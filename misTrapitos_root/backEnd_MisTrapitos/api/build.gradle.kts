88ba1bf0bc3064aa720b4ad4569e7cffe1c2a01f
NarrowSito <none@local>
2026-05-15 18:38:33 -0600

    add backend module with the spring boot template

---
+++ misTrapitos_root/backEnd_MisTrapitos/api/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.mistrapitos"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	//implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	//runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
