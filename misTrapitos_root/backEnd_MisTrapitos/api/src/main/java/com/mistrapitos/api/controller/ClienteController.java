4263378d789eeaba3a9a1cbc5ddf631e43330f34
NarrowSito <none@local>
2026-05-17 21:59:21 -0600

    new controller added, every controller now can accept petitions from localhost but from another port with crossorigin anotation

---
+++ misTrapitos_root/backEnd_MisTrapitos/api/src/main/java/com/mistrapitos/api/controller/ClienteController.java
@@ -0,0 +1,39 @@
+package com.mistrapitos.api.controller;
+
+import com.mistrapitos.api.table.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ClienteController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/{pageNumber}")
+    public List<Cliente> getClientes(@PathVariable("pageNumber") int pageNumber){
+        String sqlQuery = "select * from clientes limit 10 offset 10*" + (pageNumber-1);
+        return jdbcTemplate.query(sqlQuery,
+                (resultSet, rowNum) -> {
+                    Cliente cliente = new Cliente();
+                    cliente.setId(resultSet.getInt("id"));
+                    cliente.setNombre(resultSet.getString("nombre"));
+                    cliente.setDireccion(resultSet.getString("direccion"));
+                    cliente.setEmail(resultSet.getString("email"));
+                    cliente.setTelefono(resultSet.getString("telefono"));
+                    return cliente;
+                });
+    }
+
+    @PostMapping("/")
+    public boolean addNewCliente(@RequestBody Cliente cliente){
+        String queryString = "INSERT INTO clientes (nombre , direccion , email , telefono) VALUES (?,?,?,?)";
+        return jdbcTemplate.update(
+                queryString,cliente.getNombre(),
+                cliente.getDireccion(), cliente.getEmail(), cliente.getTelefono()) > 0;
+    }
+}
